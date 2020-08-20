437a1d56b5cf887b494fa042298213c5a9dd2942
mohammad saadat <m.sdt1376@gmail.com>
2020-08-20 21:08:33 +0430

    implement HomePresentationLogic for present data

--- FourSquare-ios/FourSquare-ios/Modules/Home/HomePresenter.swift
+++ FourSquare-ios/FourSquare-ios/Modules/Home/HomePresenter.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 protocol HomePresentationLogic {
-    //    func presentSomething(response: Home.Something.Response)
+    func presentPermisionLocationAlert(response: Home.Location.Response)
+    func presentError(response: Home.ModuleError.Response)
+    func presentLoading(response: Home.Loading.Response)
+    func presentFooterLoading(response: Home.pagination.Response)
+    func hideFooterLoading(response: Home.pagination.Response)
+    func hideLoading(response: Home.Loading.Response)
+    func presentData(response: Home.List.Response)
 }
 
 class HomePresenter {
@@ -32,10 +38,60 @@ class HomePresenter {
 // MARK: - Methods
 
 // MARK: Private
-private extension HomePresenter {}
+private extension HomePresenter {
+    func createSections(places: [Venue]) -> SectionViewModel {
+           let cells = places.map { ItemCellViewModel(place: $0) }
+           let section = DefaultSection(cells: cells)
+           return section
+       }
+}
 
 // MARK: Public
 extension HomePresenter {}
 
 // MARK: - Presentation Logic
-extension HomePresenter: HomePresentationLogic {}
+extension HomePresenter: HomePresentationLogic {
+    func hideFooterLoading(response: Home.pagination.Response) {
+           DispatchQueue.main.async {
+               self.viewController?.hideFooterLoading(viewModel: Home.pagination.ViewModel())
+           }
+       }
+       
+       func presentFooterLoading(response: Home.pagination.Response) {
+           DispatchQueue.main.async {
+               self.viewController?.displayFooterLoading(viewModel: Home.pagination.ViewModel())
+           }
+       }
+       
+       func presentData(response: Home.List.Response) {
+           let section = createSections(places: response.places)
+           let viewModel = Home.List.ViewModel(section: [section])
+           DispatchQueue.main.async {
+               self.viewController?.displayList(viewModel: viewModel)
+           }
+       }
+       
+       func presentLoading(response: Home.Loading.Response) {
+           DispatchQueue.main.async {
+               self.viewController?.displayLoading()
+           }
+       }
+       
+       func hideLoading(response: Home.Loading.Response) {
+           DispatchQueue.main.async {
+               self.viewController?.hideLoading()
+           }
+       }
+       
+       func presentError(response: Home.ModuleError.Response) {
+           DispatchQueue.main.async {
+               self.viewController?.displayError(viewModel: Home.ModuleError.ViewModel(error: response.error))
+           }
+       }
+       
+       func presentPermisionLocationAlert(response: Home.Location.Response) {
+           DispatchQueue.main.async {
+               self.viewController?.displayPermisionLocationAlert(viewModel: Home.Location.ViewModel())
+           }
+       }
+}
